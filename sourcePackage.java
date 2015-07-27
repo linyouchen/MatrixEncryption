ff45667312320a3c6f451b9c94439bfc5192ebc2
linyouchen <linyouchen.ncku@gmail.com>
2015-07-27 16:46:14 +0800

    put the current version to gitfub
    first try

---
+++ sourcePackage.java
@@ -0,0 +1,42 @@
+package encoding_v2;
+
+import java.io.Serializable;
+
+public class sourcePackage implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private double[] D = {0};
+	private int[] F = {0};
+
+	
+	public sourcePackage(double[] newD, int[] newF) {
+		D = newD;
+		F = newF;
+	}
+	
+	
+	double[] getD() 
+	{
+		return D;
+	}
+	int[] getF() 
+	{
+		return F;
+	}
+
+	void setD(double[] newD)
+	{
+		D = newD;
+	}
+	
+	void setF(int[] newF)
+	{
+		F = newF;
+	}
+
+	
+}
