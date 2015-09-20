8698221609ddd40f364975baa9bd3e6fe7a1f9e1
linyouchen <linyouchen.ncku@gmail.com>
2015-09-20 18:09:32 +0800

    06:09 still working in lab

--- src/test/java/sourcePackageTest.java
+++ src/test/java/sourcePackageTest.java
@@ -16,6 +16,13 @@ public class sourcePackageTest {
         Assert.assertArrayEquals(sourceD, test.getD(), 0.0001);
         Assert.assertArrayEquals(sourceF, test.getF());
         
+        double[] newD = {0.02, 0.04, 0.06, 0.08, 0.12};
+        int[] newF = {140,116,86,247};
+        test.setD(newD);
+        test.setF(newF);
+        Assert.assertArrayEquals(newD, test.getD(), 0.0001);
+        Assert.assertArrayEquals(newF, test.getF());
+        
 	}
 
 }
